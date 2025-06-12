4d1821a94999105d08c9ac6d6569a2aebde8d221
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-06-12 10:38:19 -0300

    FEAT: Criei a tabela turma #4

--- gestaoEscolar/admin.py
+++ gestaoEscolar/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import aluno, matricula
+from .models import aluno, matricula, turma
 
 
 admin.site.register(aluno)
 admin.site.register(matricula)
+admin.site.register(turma)
 # Register your models here.
