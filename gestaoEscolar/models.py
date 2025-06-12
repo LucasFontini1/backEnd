35714f5ea52f1536659b6f0ba97018d3c233703d
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-06-12 10:32:50 -0300

    FEAT: Criei a tabela matricula e fiz a makemigrations e migrate #2

--- gestaoEscolar/models.py
+++ gestaoEscolar/models.py
@@ -6,6 +6,16 @@ class aluno(models.Model):
     data_nascimento = models.DateField(blank=True, null=True)
     email = models.EmailField(max_length=254, blank=True, null=True)
     endereco = models.CharField(max_length=255, blank=True, null=True)
+    matricula = models.ForeignKey('matricula', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"{self.nome} "
+    
+class matricula(models.Model):
+    data_matricula = models.DateField(auto_now_add=True, blank=True, null=True)
+    status = models.CharField(max_length=20, choices=[('ativo', 'Ativo'), ('inativo', 'Inativo')], default='ativo', blank=True, null=True)
+    numero = models.CharField(max_length=20, unique=True, blank=True, null=True)
+
+
+    def __str__(self):
+        return f"{self.aluno.nome} - {self.numero} - {self.status}"
