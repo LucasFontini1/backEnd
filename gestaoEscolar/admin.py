f24416d320e2f87b9a2793d064bfab82ac85193a
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-06-12 10:18:12 -0300

    FEAT: Criação da tabela aluno, e registra-lo no admin.py #3

--- gestaoEscolar/admin.py
+++ gestaoEscolar/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import aluno
 
+
+admin.site.register(aluno)
 # Register your models here.
--- gestaoEscolar/models.py
+++ gestaoEscolar/models.py
@@ -1,2 +1,12 @@
+from django.db import models
 
+class aluno(models.Model):
+    cpf = models.CharField(max_length=11, primary_key=True)
+    nome = models.CharField(max_length=100, blank=True, null=True)
+    data_nascimento = models.DateField(blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    endereco = models.CharField(max_length=255, blank=True, null=True)
+    matricula = models.ForeignKey('matricula', on_delete=models.CASCADE, blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.nome} "
