edeb48ec65968a9b2a3dae68fad48a9df3448667
LauraEloysa <lauraeloysad@gmail.com>
2025-06-22 19:28:17 -0300

    FEAT: Adiconei a tabela professores #9

--- gestaoEscolar/admin.py
+++ gestaoEscolar/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import aluno, matricula, turma
+from .models import aluno, matricula, turma, professores
 
 
 admin.site.register(aluno)
 admin.site.register(matricula)
 admin.site.register(turma)
+admin.site.register(professores)
 # Register your models here.
--- gestaoEscolar/models.py
+++ gestaoEscolar/models.py
@@ -27,3 +27,13 @@ class turma(models.Model):
 
     def __str__(self):
         return f"{self.nome} - {self.ano} - {self.periodo}"
+    
+class professores(models.Model):
+    cpf = models.CharField(max_length=11, primary_key=True)
+    nome = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    data_nascimento = models.DateField(blank=True, null=True)
+    endereco = models.CharField(max_length=255, blank=True, null=True)
+
+
+    
