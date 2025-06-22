357a824d70e31f05dff38e8e12b44d12ae459c9d
LauraEloysa <lauraeloysad@gmail.com>
2025-06-22 19:51:39 -0300

    FEAT:  adicionando a tbela curso #10

--- gestaoEscolar/models.py
+++ gestaoEscolar/models.py
@@ -26,7 +26,7 @@ class turma(models.Model):
     periodo = models.CharField(max_length=20, choices=[('manhã', 'Manhã'), ('tarde', 'Tarde'), ('noite', 'Noite')], blank=True, null=True)
 
     def __str__(self):
-        return f"{self.nome} - {self.ano} - {self.periodo}"
+        return f"{self.nome} - {self.anoRegistro} - {self.periodo}"
     
 class professores(models.Model):
     cpf = models.CharField(max_length=11, primary_key=True)
@@ -35,5 +35,15 @@ class professores(models.Model):
     data_nascimento = models.DateField(blank=True, null=True)
     endereco = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.nome}"
 
+class curso(models.Model):
+    codigo_curso = models.CharField(max_length=50, primary_key=True)
+    nome = models.CharField(max_length=100, blank=True, null=True)
+    modalidade = models.CharField(max_length=20, choices=[('semipresencial', 'Semipresencial'), ('online', 'Online'), ('presencial', 'Presencial')])
+    carga_horaria = models.IntegerField(max_length=5, null=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.modalidade} - {self.carga_horaria} horas"
     
