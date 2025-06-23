8e6830d02aa7e0a7017166fa6e44a04cf763cf7f
LauraEloysa <lauraeloysad@gmail.com>
2025-06-22 22:29:33 -0300

    FEAT: adicionei a api coordenador #29

--- gestaoEscolar/urls.py
+++ gestaoEscolar/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AlunoViewSet, MatriculaViewSet, TurmaViewSet, ProfessoresViewSet, CursoViewSet
+from .views import AlunoViewSet, MatriculaViewSet, TurmaViewSet, ProfessoresViewSet, CursoViewSet, CoodenadorViewSet
 
 router = DefaultRouter()
 router.register(r'aluno', AlunoViewSet)
@@ -8,6 +8,7 @@ router.register(r'matricula', MatriculaViewSet)
 router.register(r'turma', TurmaViewSet)
 router.register(r'professores', ProfessoresViewSet)
 router.register(r'curso', CursoViewSet)
+router.register(r'coodenador', CoodenadorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
