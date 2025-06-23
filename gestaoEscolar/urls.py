694d0ba8360ac12d7e1b12f27d65e95c56ae311b
LauraEloysa <lauraeloysad@gmail.com>
2025-06-22 21:50:03 -0300

    FEAT: adicionei a api matricula #22

--- gestaoEscolar/urls.py
+++ gestaoEscolar/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AlunoViewSet
+from .views import AlunoViewSet, MatriculaViewSet
 
 router = DefaultRouter()
 router.register(r'aluno', AlunoViewSet)
+router.register(r'matricula', MatriculaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
